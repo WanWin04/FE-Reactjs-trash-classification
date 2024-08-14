c6c19d4ec6fec50ccca763ba574cd26f3217e2b9
Nguyen Trong Kha <trongkha08022k4@gmail.com>
2024-08-15 01:14:13 +0700

    finish login-reg-4gotpass

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,11 +1,21 @@
 import './header.css';
+import { FaBars } from "react-icons/fa";
+
+
 function Header() {
+
+    const func = () => {
+        const hiddenFunc = document.querySelector('.hidden-func');
+        hiddenFunc.classList.toggle('show');
+    }
+
+
     return (
         <div id="header">
             <div id="logo-zone">
-            <button id="logo"><img src="#" alt="logo"></img></button>
-            <h2>XXXXXXXXXX</h2>
+                <button id="logo"><img src="#" alt="logo"></img></button>
             </div>
+            <h2>XXXX</h2>
             <div id="func-head">
             <div>TRANG CHỦ</div>
             <div>ĐỐI TÁC</div>
@@ -14,7 +24,20 @@ function Header() {
             <div>NGHỀ NGHIỆP</div>
             <div>VỀ CHÚNG TÔI</div>
             </div>
-            <button className="btn">Đăng nhập</button>
+            <ul className="hidden-func">
+                <li>TRANG CHỦ</li>
+                <li>ĐỐI TÁC</li>
+                <li>TIN CMC</li>
+                <li>CỔ ĐÔNG</li>
+                <li>NGHỀ NGHIỆP</li>
+                <li>VỀ CHÚNG TÔI</li>
+            </ul>
+            <div id="mobile-func" onClick={func}>
+                <i>
+                    <FaBars/>
+                    </i>
+            </div>
+            <button className="bton">Đăng nhập</button>
         </div>
     );
 }
