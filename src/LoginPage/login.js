bdf698772985988cba132c6389e53e4984bb4245
Nguyen Trong Kha <trongkha08022k4@gmail.com>
2024-08-23 23:20:47 +0700

    fixed layout

--- src/LoginPage/login.js
+++ src/LoginPage/login.js
@@ -88,9 +88,9 @@ function LoginPage() {
                     <div className='form-box forgotpass'>
                         <h2 className="animation" style={{'--i':10, '--j':0}}>RESET PASSWORD</h2>
                         <form action="#" className='forgotForm'>
-                            <button type="submit" className="btn animation" style={{'--i':11, '--j':1}} onClick={LogAnd4got}>EMAIL</button>
-                            <button type="submit" className="btn animation" style={{'--i':12, '--j':2}} onClick={LogAnd4got}>OTP</button>
-                            <button type="submit" className="btn animation" style={{'--i':13, '--j':3}} onClick={LogAnd4got}>CAPCHA</button>
+                            <button type="submit" className="butn animation" style={{'--i':11, '--j':1}} onClick={LogAnd4got}>EMAIL</button>
+                            <button type="submit" className="butn animation" style={{'--i':12, '--j':2}} onClick={LogAnd4got}>OTP</button>
+                            <button type="submit" className="butn animation" style={{'--i':13, '--j':3}} onClick={LogAnd4got}>CAPCHA</button>
                             <div className='backtolog animation'  style={{'--i': 14, '--j':4}} onClick={LogAnd4got}>
                                 Về trang đăng nhập
                             </div>
