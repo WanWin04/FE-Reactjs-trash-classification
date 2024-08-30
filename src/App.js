6675b5077054c08ffb30dab50642a0f224673723
Khang Le Duy <khangleduy@Khangs-MacBook-Air.local>
2024-08-30 15:38:11 +0700

    ERR

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Header from './Header/header';
 import LoginPage from './LoginPage/login';
+import Hello from './components/hello';
+import ProtectedRoute from './components/ProtectedRoute';
 import './App.css';
-
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from './const';
 function App() {
-    
+    function Logout(){
+        localStorage.removeItem(ACCESS_TOKEN);
+        localStorage.removeItem(REFRESH_TOKEN);
+    }
     return (
-        <div>
-            <Header />
-            <LoginPage />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={
+                    <Hello />
+                } />
+                <Route path='/login' element={<><Header /><LoginPage/></>} />
+                <Route path='/logout' element={<Logout />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
